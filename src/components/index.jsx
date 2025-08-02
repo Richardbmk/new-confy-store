ff2035e5fa56819de32ec3903346bc649135240e
Richardbmk <rdobmk@gmail.com>
2025-08-02 22:16:14 +0200

    Finish with Navbar and theme feature

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,8 @@
 import ErrorElement from './ErrorElement'
 import FormInput from './FormInput'
 import SubmitBtn from './SubmitBtn'
+import Header from './Header'
+import Navbar from './Navbar'
+import NavLinks from './NavLinks'
 
-export { ErrorElement, FormInput, SubmitBtn }
+export { ErrorElement, FormInput, SubmitBtn, Header, Navbar, NavLinks }
