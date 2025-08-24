e2f4105625b5e57cc25f4f34b1ea4e6ca8b46ada
Richardbmk <rdobmk@gmail.com>
2025-08-24 19:40:18 +0200

    Cart page in progress

--- src/components/CartItemsList.jsx
+++ src/components/CartItemsList.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem'
 
 const CartItemsList = () => {
-  return <div>This is the Cart Items List</div>
+  const cartItems = useSelector((state) => state.cartState.cartItems)
+  //   console.log(cartItems)
+
+  return (
+    <div>
+      {cartItems.map((item) => {
+        return <CartItem key={item.cartID} cartItem={item} />
+      })}
+    </div>
+  )
 }
 
 export default CartItemsList
